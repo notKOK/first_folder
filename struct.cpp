d0af2bbbd27d299470218be0f7c21857e2e0acb6
Danila Anosov <a.deni.i@mail.ru>
2019-12-22 18:56:38 +0300

    forgotten struct

--- struct.cpp
+++ struct.cpp
@@ -2,10 +2,92 @@
 
 using namespace std;
 
+struct Alignment //default alignment
+{
+	int a;
+
+	short b;
+
+	double c;
+};
+
+#pragma pack(push, 1)
+struct NoAlignment // one byte alignment
+{
+	int a;
+
+	short b;
+
+	double c;
+};
+#pragma pack(pop)
+
+void print(Alignment smth);
+
+void print1(NoAlignment smth);
+
+void size_alig(Alignment smth);
+
+void size_noalig(NoAlignment smth);
+
 int main()
 {
+	Alignment test;
+
+	NoAlignment test1;
+
+	cout << "Location with alignment: " << endl;
+
+	print(test);
+
+	cout << "Location without alignment: " << endl;
+
+	print1(test1);
+
+	size_alig(test);
+
+	size_noalig(test1);
+
 	return 0;
 
 }
 
+void print(Alignment smth)
+{
+	cout << &(smth.a) << endl;
 
+	cout << &(smth.b) << endl;
+
+	cout << &(smth.c) << endl;
+}
+
+void print1(NoAlignment smth)
+{
+	cout << &(smth.a) << endl;
+
+	cout << &(smth.b) << endl;
+
+	cout << &(smth.c) << endl;
+}
+
+void size_alig(Alignment smth)
+{
+	int size;
+
+	size = sizeof(smth.a) + sizeof(smth.b) + sizeof(smth.c);
+
+	cout << "Size of structure from sum of fields: " << size << endl;
+
+	cout << "Size of structure from 'sizeof': " << sizeof(smth) << endl;
+}
+
+void size_noalig(NoAlignment smth)
+{
+	int size;
+
+	size = sizeof(smth.a) + sizeof(smth.b) + sizeof(smth.c);
+
+	cout << "Size of structure from sum of fields: " << size << endl;
+
+	cout << "Size of structure from 'sizeof': " << sizeof(smth) << endl;
+}
